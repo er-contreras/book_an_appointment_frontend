0b83d8daba6f9be174e15671e732c58b40bd4fe4
Erick Contreras <eri89--@hotmail.com>
2022-01-12 17:34:02 -0600

    add Link to redirect after click the button

--- src/components/login.js
+++ src/components/login.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import '../styles/Login.css';
+import { Link } from 'react-router-dom';
 
 export default function Login() {
+  // const [username, setUserName] = useState('');
+
+  // function handleSubmit(event) {
+  //   event.preventDefault();
+  // }
+
   return (
     <div className="login-wrapper">
       <h1>Please Log In</h1>
@@ -11,14 +18,24 @@ export default function Login() {
           <input type="text" id="username" />
         </label>
 
-        <label htmlFor="password">
+        {/* <label htmlFor="username">
+          <p>Username</p>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        </label> */}
+
+        {/* <label htmlFor="password">
           <p>Password</p>
           <input type="password" id="password" />
-        </label>
+        </label> */}
 
-        <div>
-          <button type="submit">Submit</button>
-        </div>
+        <Link to="/home">
+          <button type="submit">Login</button>
+        </Link>
       </form>
     </div>
   );
