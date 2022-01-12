81315b07997b1e4ea74c7c03521153438dc55282
Erick Contreras <eri89--@hotmail.com>
2022-01-12 10:26:00 -0600

    Add path to dashboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './styles/App.css';
 import { Route, Routes } from 'react-router-dom';
 import Yachts from './components/yachts';
+import Dashboard from './components/dashboard';
 
 function App() {
   return (
     <Routes>
-      <Route exact path="/" element={<Yachts />} />
+      <Route exact path="/" element={<Dashboard />} />
+      <Route path="/home" element={<Yachts />} />
     </Routes>
   );
 }
