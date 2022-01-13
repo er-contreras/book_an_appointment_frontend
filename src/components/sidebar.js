f5adf27ef351a73c1e6230f54021eadfc5a162f1
Erick Contreras <eri89--@hotmail.com>
2022-01-13 10:31:45 -0600

    Set sidebar dimensions

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,29 +1,27 @@
 import '../styles/Sidebar.css';
 
 const Sidebar = () => (
-  <div id="sidebar-page">
-    <div id="sidebar-content">
-      <div>
-        <h2>Yachts!</h2>
-      </div>
+  <div id="sidebar-content">
+    <div className="navigation-title">
+      <h2>Yachts!</h2>
+    </div>
 
-      <ul className="navigation-panel">
-        <li>Yacht</li>
-        <li>Reserve form</li>
-        <li>My Reservations</li>
-        <li>Add Yacht</li>
-        <li>Delete Yacht</li>
+    <ul className="navigation-panel">
+      <li>YACHTS</li>
+      <li>RESERVE FORM</li>
+      <li>MY RESERVATIONS</li>
+      <li>ADD YACHT</li>
+      <li>DELETE YACHT</li>
+    </ul>
+
+    <div className="bottom-panel">
+      <ul className="icons-panel">
+        <li>Twitter</li>
+        <li>Facebook</li>
+        <li>Google</li>
+        <li>Vimeo</li>
+        <li>Pinterest</li>
       </ul>
-
-      <div className="bottom-panel">
-        <ul className="icons-panel">
-          <li>Twitter</li>
-          <li>Facebook</li>
-          <li>Google</li>
-          <li>Vimeo</li>
-          <li>Pinterest</li>
-        </ul>
-      </div>
     </div>
   </div>
 );
