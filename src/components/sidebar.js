aa10f7f9190acb1089aa988e9d0e70579c066312
Erick Contreras <eri89--@hotmail.com>
2022-01-15 18:39:49 -0600

    Style sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,4 +1,10 @@
 import '../styles/Sidebar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import
+{
+  faTwitter, faFacebook, faGooglePlus, faVimeo, faPinterest,
+} from '@fortawesome/free-brands-svg-icons';
+import { faCopyright } from '@fortawesome/free-solid-svg-icons';
 
 const Sidebar = () => (
   <div id="sidebar-content">
@@ -7,7 +13,7 @@ const Sidebar = () => (
     </div>
 
     <ul className="navigation-panel">
-      <li>YACHTS</li>
+      <li className="yachts-background">YACHTS</li>
       <li>RESERVE FORM</li>
       <li>MY RESERVATIONS</li>
       <li>ADD YACHT</li>
@@ -16,12 +22,16 @@ const Sidebar = () => (
 
     <div className="bottom-panel">
       <ul className="icons-panel">
-        <li>Twitter</li>
-        <li>Facebook</li>
-        <li>Google</li>
-        <li>Vimeo</li>
-        <li>Pinterest</li>
+        <FontAwesomeIcon icon={faTwitter} />
+        <FontAwesomeIcon icon={faFacebook} />
+        <FontAwesomeIcon icon={faGooglePlus} />
+        <FontAwesomeIcon icon={faVimeo} />
+        <FontAwesomeIcon icon={faPinterest} />
       </ul>
+      <div>
+        <FontAwesomeIcon icon={faCopyright} />
+        2021 PLAGGIO & C.S.P.A - PIVA
+      </div>
     </div>
   </div>
 );
